d3c9fc4c15cad047da4f42c5395f95e3933fba65
Anagha-20 <989anaghasm@gmail.com>
2021-12-20 02:19:05 +0530

    minor updates added

--- Onedialect.py
+++ Onedialect.py
@@ -1,15 +1,20 @@
 import speech_recognition as sr
+import function as f
 r = sr.Recognizer()
-  
+
+# 1 thread to listen, another for vibrator, like a buffered stream 
+# 1 thread for button and similarly the speaker works
 
 with sr.Microphone() as source:
-  print("Talk")
-  audio = r.listen(source)
-  print("end")
-
-  try:
-        # using google speech recognition
-        text = r.recognize_google(audio)
-        print(text)
-  except:
-         print("Sorry, I did not get that")
+  
+  while True: 
+      audio = r.listen(source)
+      try:
+            # using google speech recognition
+            text = r.recognize_google(audio)
+            print(text)
+            morse = f.alpha_to_morse(text.upper())
+            print('Morse Code: ', morse)
+            f.morse_play(morse)
+      except:
+             print("Sorry, I did not get that")
